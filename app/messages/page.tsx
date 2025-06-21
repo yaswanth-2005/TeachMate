fb79deafc8b54d2fd865fca356812634a171bf66
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-06-21 12:00:47 +0530

    messages fixed

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
+  const router = useRouter();
   return (
-    <div className="text-center mt-5 text-blue-500">
+    <div className="text-center mt-5 text-blue-500 flex flex-col items-center justify-center">
       This feature is Under Construction.
+      <div className="mt-5">
+        <Button onClick={() => router.push("/dashboard")}>
+          {" "}
+          <ChevronLeft />
+          Back to Dashboard
+        </Button>
+      </div>
     </div>
   );
 };
