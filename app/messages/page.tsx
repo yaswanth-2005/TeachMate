b8639c92c931d0a7d3ad692cb7a37154267a809f
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-06-21 11:53:06 +0530

    messages under Development added

---
+++ app/messages/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="text-center mt-5 text-blue-500">
+      This feature is Under Construction.
+    </div>
+  );
+};
+
+export default page;
